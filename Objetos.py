0c3e5ef1bc2df835fc5dc218bbba64b21615debf
myriam120 <4422399895mg@gmail.com>
2023-02-22 18:36:30 -0600

    Trabajamos personaje con su constructor

--- Objetos.py
+++ Objetos.py
@@ -2,11 +2,29 @@
 #1. importar las clases 
 from Personaje import *
 
-#2. instanciar un objeto 
-Heroe = personaje() 
+#2. solicitar atributo para el objeto 
+print("")
+print("### Solicitud de datos del Heroe ###")
+espHeroe = input("Escribe el nombre de la especie: ")
+nomHeroe = input("Escribe el nombre del heroe: ")
+alturaHeroe = float(input("Escribe la altura del heroe: "))
+cargaHeroe = int(input("Cuantas balas se regargan al heroe: "))
 
-#3. accedera a sus atributos 
-print("Atributo personaje")
+print("")
+print("### Solicitud de datos del villano ###")
+espV = input("Escribe el nombre de la especie del villano: ")
+nomV = input("Escribe el nombre del villano: ")
+alturaV = float(input("Escribe la altura del villano: "))
+cargaV = int(input("Cuantas balas se regargan al villano: "))
+
+#3. creamos 2 objetos 
+Heroe = personaje(espHeroe, nomHeroe, alturaHeroe) 
+Villano = personaje (espV, nomV, alturaV)
+
+
+#4. accedera a sus atributos y metodos de cada obj
+print("")
+print("### Atributos y metodos del Heroe ###")
 print("El personaje pertenece a la raza: "+ Heroe.expecie )
 print("Se llama: "+ Heroe.nombre )
 print("Mide: "+ str(Heroe.altura) + "Metros" )
@@ -15,5 +33,18 @@ print("")
 print("Metodos personaje")
 Heroe.correr(True)
 Heroe.lanzarGranada()
-Heroe.recargarArma(68)
+Heroe.recargarArma(cargaHeroe)
+
+print("")
+print("### Atributos y metodos del villano ###")
+print("El personaje pertenece a la raza: "+ Villano.expecie )
+print("Se llama: "+ Villano.nombre )
+print("Mide: "+ str(Villano.altura) + "Metros" )
+print("")
+
+print("Metodos personaje")
+Villano.correr(True)
+Villano.lanzarGranada()
+Villano.recargarArma(cargaV)
+
 
--- Personaje.py
+++ Personaje.py
@@ -1,22 +1,24 @@
 
 class personaje:
-    #atributos del personaje 
-    especie = "humano"
-    nombre = "debora"
-    altura = 1.70
+    #creamos al constructor 
+    def __init__(self, esp, nom, altura):
+        self.especie = esp
+        self.nombre = nom
+        self.altura = altura
+    
     
     #metodos de persoaje 
     def correr (self, status):
         if (status):
             print ("El personaje " + self.nombre + "Esta corriendo " )
-            else 
-             print ("El personaje " + self.nombre + "Se detuvo " )
+        else:
+            print ("El personaje " + self.nombre + "Se detuvo " )
             
     def lanzarGranada(self):
         print("Se lanzo la granada ")
         
-    def recargarArma(self, muiciones):
+    def recargarArma(self, municiones):
         cargador  = 5 
-        cargador = cargadot + municiones 
+        cargador = cargador + municiones 
         print("El arma tiene " + cargador + "balas")    
              
