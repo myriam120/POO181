3c8883763406b579bb440367ff58b0e527486e48
myriam120 <4422399895mg@gmail.com>
2023-02-24 18:25:19 -0600

    Ejemplo de encapsulamiento

--- Objetos.py
+++ Objetos.py
@@ -21,25 +21,28 @@ cargaV = int(input("Cuantas balas se regargan al villano: "))
 Heroe = personaje(espHeroe, nomHeroe, alturaHeroe) 
 Villano = personaje (espV, nomV, alturaV)
 
+Heroe.setnombre("Pepe pecas")
 
 #4. accedera a sus atributos y metodos de cada obj
+
 print("")
 print("### Atributos y metodos del Heroe ###")
-print("El personaje pertenece a la raza: "+ Heroe.expecie )
-print("Se llama: "+ Heroe.nombre )
-print("Mide: "+ str(Heroe.altura) + "Metros" )
+print("El personaje pertenece a la raza: "+ Heroe.getespecie() )
+print("Se llama: "+ Heroe.getnombre() )
+print("Mide: "+ str(Heroe.getaltura()) + "Metros" )
 print("")
 
 print("Metodos personaje")
 Heroe.correr(True)
 Heroe.lanzarGranada()
 Heroe.recargarArma(cargaHeroe)
+Heroe.__pensar()
 
 print("")
 print("### Atributos y metodos del villano ###")
-print("El personaje pertenece a la raza: "+ Villano.expecie )
-print("Se llama: "+ Villano.nombre )
-print("Mide: "+ str(Villano.altura) + "Metros" )
+print("El personaje pertenece a la raza: "+ Villano.getespecie() )
+print("Se llama: "+ Villano.getnombre() )
+print("Mide: "+ str(Villano.getaltura()) + "Metros" )
 print("")
 
 print("Metodos personaje")
--- Personaje.py
+++ Personaje.py
@@ -1,18 +1,18 @@
 
 class personaje:
     #creamos al constructor 
-    def __init__(self, esp, nom, altura):
-        self.especie = esp
-        self.nombre = nom
-        self.altura = altura
+    def __init__(self, esp, nom, alt):
+        self.__especie = esp
+        self.__nombre = nom
+        self.__altura = alt
     
     
     #metodos de persoaje 
     def correr (self, status):
         if (status):
-            print ("El personaje " + self.nombre + "Esta corriendo " )
+            print ("El personaje " + self.__nombre + "Esta corriendo " )
         else:
-            print ("El personaje " + self.nombre + "Se detuvo " )
+            print ("El personaje " + self.__nombre + "Se detuvo " )
             
     def lanzarGranada(self):
         print("Se lanzo la granada ")
@@ -20,5 +20,20 @@ class personaje:
     def recargarArma(self, municiones):
         cargador  = 5 
         cargador = cargador + municiones 
-        print("El arma tiene " + cargador + "balas")    
+        print("El arma tiene " + str(cargador) + "balas")   
+        
+    def getespecie(self):
+        return self.__especie 
+    def setespecie(self, esp):
+        self.__especie = esp 
+        
+    def getnombre(self):
+        return self.__nombre 
+    def setnombre(self, nom):
+        self.__nombre = nom 
+        
+    def getaltura(self):
+        return self.__altura
+    def setaltura(self, alt):
+        self.__altura = alt    
              
