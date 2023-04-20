def0e1d0e9feae3161efd4d2146acee05266f412
myriam120 <4422399895mg@gmail.com>
2023-04-19 18:54:44 -0600

    Tercer commit

--- tkinther/__pycache__/tkinterSQlite/Examen3/Constructor.py
+++ tkinther/__pycache__/tkinterSQlite/Examen3/Constructor.py
@@ -18,18 +18,65 @@ class Controlador:
         except sqlite3.OperationalError:
             print('No se pudo conectar')
             
-    def usuario(self, Nom, Cor, Con):
+    def usuario(self, idm, ma, ca):
         Conx = self.conexionBD()
         
-        if Nom == '' or Cor == '' or Con == '':
+        if idm == '' or ma == '' or ca == '':
             messagebox.showwarning('Aguas', 'Formulario Incompleto')
         else:
             Cursor = Conx.cursor()  
-            ConH = self.encriptarCon(Con)
-            Datos = (Nom, Cor, ConH)
-            qrInsert = 'INSERT INTO TBRegistrados(nombre, correo, contra) VALUES (?, ?, ?)' 
+            ConH = self.encriptarCon(ma)
+            Datos = (idm, ca, ma)
+            qrInsert = 'INSERT INTO MatConstuccion (id, material, cantidad) VALUES (?, ?, ?)' 
             
             Cursor.execute(qrInsert, Datos) 
             Conx.commit()
             Conx.close()
-            messagebox.showinfo('Éxito', 'Usuario Guardado') 
+            messagebox.showinfo('Éxito', 'Materiales Guardados') 
+   
+    def consultarMaterial (self, id):
+        #1. Prepara una conexion 
+        conx= self.conexionBD()
+        #2 verificar si el id contiene algo
+        if (id == ""):
+            messagebox.showwarning("Cuidado", "Id vacio escribe algo valido")
+        else:
+            try:
+                #preparar el cursor del query
+                cursor = conx.cursor()
+                selectQry = "select * from MatConstuccion where id= "+ id
+                
+                #4. ejecutar y guardar la consulta
+                cursor.execute(selectQry)
+                rsUsuario = cursor.fetchall()
+                conx.close()
+                
+                return rsUsuario
+             
+            except sqlite3.OperationalError:
+                print("Error consulta")   
+                
+    def ActualizarMaterial(self, id,ma,ca):
+        #1. usamos una conexion 
+        conx=self.conexionBD()
+        
+        #2. validar parámetros vacíos
+        
+        if(id=="" or ma=="" or ca==""):
+            messagebox.showwarning("Campos incompletos")
+            return False
+        if ma not in ["Sí", "No"]:
+            messagebox.showwarning("Error","El campo de disponibilidad solo admite sí o No, favor de seleccionar una opción válida")
+            return False
+        try:
+            float(ca)
+            cursor= conx.cursor()
+            datosUP=(id,ma,ca)
+            qrUPD="UPDATE MatConstuccion SET IDMat=?, Materail=?, Cantidad=? Where IDMat="+id
+            cursor.execute(qrUPD,datosUP)           
+            conx.commit()
+            messagebox.showinfo("Exito","Material Actualizado")
+            return True
+        except ValueError:
+            messagebox.showwarning("Error")
+            return False
--- tkinther/__pycache__/tkinterSQlite/Examen3/usuario.py
+++ tkinther/__pycache__/tkinterSQlite/Examen3/usuario.py
@@ -1,15 +1,13 @@
-import threading
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 from Constructor  import *
 
-construrctor = Controlador()
+constrolador = Controlador()
 
 def ejecutaIn():
-    construrctor.usuario(varNom.get(),varCor.get(),varCon.get())
-
-
+    constrolador.usuario(varID.get(),varMA.get(),varCA.get())
+      
 Ventana= Tk()
 Ventana.title("FERRETERIA")
 Ventana.geometry("300x300")
@@ -19,28 +17,30 @@ panel.pack(fill="both",expand="yes")
 
 Pestaña1= ttk.Frame(panel)
 Pestaña2= ttk.Frame(panel)
-Pestaña3= ttk.Frame(panel)
 
+#1. Pestaña 1 
 titulo= Label(Pestaña1,text="Registro de Material", fg="purple", font=("Modern",18)).pack()
 
-varNom= tk.StringVar()
-lblNom= Label(Pestaña1, text="IDMAT: ").pack()
-txtNom= Entry(Pestaña1, textvariable=varNom).pack()
+varID= tk.StringVar()
+lblID= Label(Pestaña1, text="IDMat: ").pack()
+txtID= Entry(Pestaña1, textvariable=varID).pack()
 
-varCor= tk.StringVar()
-lblCor= Label(Pestaña1, text="Material: ").pack()
-txtCor= Entry(Pestaña1, textvariable=varCor).pack()
+varMA= tk.StringVar()
+lblMA= Label(Pestaña1, text="Material: ").pack()
+txtMA= Entry(Pestaña1, textvariable=varMA).pack()
 
-varCon= tk.StringVar()
-lblCon= Label(Pestaña1, text="Cantidad: ").pack()
-txtCon= Entry(Pestaña1, textvariable=varCon).pack()
+varCA= tk.StringVar()
+lblCA= Label(Pestaña1, text="Cantidad: ").pack()
+txtCA= Entry(Pestaña1, textvariable=varCA).pack()
 
 btnGuardar= Button(Pestaña1, text="Guardar", command=ejecutaIn)
+
+#2. pestaña dos 
 
 titulo2= Label(Pestaña2,text="Actualizar Material", fg="Purple", font=("Modern",18)).pack()
 
 varBus= tk.StringVar()
-lblid= Label(Pestaña2, text="Identificador de Usuario: ").pack()
+lblid= Label(Pestaña2, text="Identificador de Material").pack()
 txtid= Entry(Pestaña2, textvariable=varBus).pack()
 btnBus= Button(Pestaña2, text="Buscar").pack()
 
@@ -48,19 +48,6 @@ subBus=Label(Pestaña2,text="Encontrado", fg="green", font=("Modern",15))
 textEnc= tk.Text(Pestaña2,height=5,width=52)
 textEnc.pack()
 
-#pestaña3: Consultar usuario
-
-titulo3 = Label(Pestaña3, text="Todos los Materiales", fg="Purple", font=("Modern", 18)).pack
-
-vartod= tk.StringVar()
-lblou= Label(Pestaña3, text="Mostrar todo los materiales ", fg="Purple", font=("Modern", 18)).pack()
-btntod= Button(Pestaña3, text="Buscar").pack()
-
-
-tree = ttk.Treeview(Pestaña3,columns=("idmat", "material", "Cantidad"))
-tree.pack(side=tk.TOP, fill=tk.BOTH, padx=5, pady=5)
-tree.heading("#1", text="idmat")
-tree.heading("#2", text="material")
-tree.heading("#3", text="Cantidad")
-
+panel.add(Pestaña1, text="Formulario de Materiales")
+panel.add(Pestaña2, text="Buscar Materiales")
 Ventana.mainloop()
